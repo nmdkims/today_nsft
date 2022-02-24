1d94afb4d7c334f4f3608cfb129a6ac8a68a2758
KIMHOONHEE <nmdkims@gmail.com>
2022-02-24 16:52:12 +0900

    수정 및 추가 - 회원가입 로그인 기능 수정, 로그아웃 기능 추가 #2

--- config/views.py
+++ config/views.py
@@ -1,10 +1,117 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from content.models import Feed, Reply, FeedLike, Bookmark
+from user.models import User, Follow
 
 
 class Main(APIView):
     def get(self, request):
-        return render(request, "today_nsft/main.html")
+        email = request.session.get('email', None)
+        if email is None:
+            return render(request, 'user/login.html')
 
-    def post(self, request):
-        return render(request, "today_nsft/main.html")
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            return render(request, 'user/login.html')
+
+        feed_object_list = Feed.objects.all().order_by('-id')
+        feed_list = []
+        for feed in feed_object_list:
+            like_count = FeedLike.objects.filter(feed_id=feed.id, is_like=True).count()
+            is_like = FeedLike.objects.filter(feed_id=feed.id, is_like=True, email=email).exists()
+            is_bookmarked = Bookmark.objects.filter(email=email, is_bookmarked=True, feed_id=feed.id).exists()
+            reply_list = Reply.objects.filter(feed_id=feed.id)
+            profile_image = User.objects.filter(email=feed.email).first().thumbnail or 'default_profile.jpg'
+            feed_list.append(dict(
+                id=feed.id,
+                profile_image=profile_image,
+                user_id=feed.user_id,
+                image=feed.image,
+                content=feed.content,
+                like_count=like_count,
+                is_like=is_like,
+                reply_list=reply_list,
+                is_bookmarked=is_bookmarked
+            ))
+
+        return render(request,
+                      'today_nsft/main.html',
+                      context=dict(feed_list=feed_list,
+                                   user=user))
+
+
+class Profile(APIView):
+    def get(self, request):
+        email = request.session.get('email', None)
+        if email is None:
+            return render(request, 'user/login.html')
+
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            return render(request, 'user/login.html')
+
+        feed_object_list = Feed.objects.filter(email=email).order_by('-id')
+        feed_list = []
+        row_feed_list = []
+        feed_count = feed_object_list.count()
+        for feed in feed_object_list:
+            like_count = FeedLike.objects.filter(feed_id=feed.id, is_like=True).count()
+            is_like = FeedLike.objects.filter(feed_id=feed.id, is_like=True, email=email).exists()
+            reply_count = Reply.objects.filter(feed_id=feed.id).count()
+            row_feed_list.append(dict(
+                id=feed.id,
+                profile_image=feed.profile_image,
+                user_id=feed.user_id,
+                image=feed.image,
+                content=feed.content,
+                like_count=like_count,
+                is_like=is_like,
+                reply_count=reply_count
+            ))
+
+            if len(row_feed_list) == 3:
+                feed_list.append(dict(row_feed_list=row_feed_list))
+                row_feed_list = []
+
+        if len(row_feed_list) > 0:
+            feed_list.append(dict(row_feed_list=row_feed_list))
+
+        following_count = Follow.objects.filter(follower=email, is_live=True).count()
+        follower_count = Follow.objects.filter(following=email, is_live=True).count()
+
+        bookmark_list = Bookmark.objects.filter(email=email, is_bookmarked=True).order_by('-id')
+        bookmark_feed_list = []
+        row_bookmark_feed_list = []
+        for bookmark in bookmark_list:
+            feed = Feed.objects.filter(id=bookmark.feed_id).first()
+            if feed is None:
+                continue
+            like_count = FeedLike.objects.filter(feed_id=feed.id, is_like=True).count()
+            is_like = FeedLike.objects.filter(feed_id=feed.id, is_like=True, email=email).exists()
+            reply_count = Reply.objects.filter(feed_id=feed.id).count()
+            row_bookmark_feed_list.append(dict(
+                id=feed.id,
+                profile_image=feed.profile_image,
+                user_id=feed.user_id,
+                image=feed.image,
+                content=feed.content,
+                like_count=like_count,
+                is_like=is_like,
+                reply_count=reply_count
+            ))
+
+            if len(row_bookmark_feed_list) == 3:
+                bookmark_feed_list.append(dict(row_bookmark_feed_list=row_bookmark_feed_list))
+                row_bookmark_feed_list = []
+
+        if len(row_bookmark_feed_list) > 0:
+            bookmark_feed_list.append(dict(row_bookmark_feed_list=row_bookmark_feed_list))
+
+        return render(request,
+                      'today_nsft/profile.html',
+                      context=dict(feed_list=feed_list,
+                                   bookmark_feed_list=bookmark_feed_list,
+                                   feed_count=feed_count,
+                                   following_count=following_count,
+                                   follower_count=follower_count,
+                                   user=user))
--- user/views.py
+++ user/views.py
@@ -1,8 +1,12 @@
+import os
+from uuid import uuid4
+
 from django.contrib.auth.hashers import make_password
 from django.shortcuts import render
 from rest_framework.views import Response
 from rest_framework.views import APIView
 
+from config.settings import MEDIA_ROOT
 from user.models import User
 
 
@@ -20,7 +24,7 @@ class Join(APIView):
                             nickname=nickname,
                             name=name,
                             password=make_password(password),
-                            profile_image="default_profile.jpg")
+                            thumbnail="default_profile.jpg")
 
         return Response(status=200)
 
@@ -36,9 +40,42 @@ class Login(APIView):
         user = User.objects.filter(email=email).first()
 
         if user is None:
-            return Response(status=404, data=dict(message="회원정보가 잘못되었습니다."))
+            return Response(status=400, data=dict(message="회원정보가 잘못되었습니다."))
 
         if user.check_password(password):
+            request.session['email'] = email
             return Response(status=200)
+        else:
+            return Response(status=400, data=dict(message="회원정보가 잘못되었습니다."))
 
 
+class LogOut(APIView):
+    def get(self, request):
+        request.session.flush()
+        return render(request, 'user/login.html')
+
+
+class UpdateProfile(APIView):
+    def post(self, request):
+        email = request.session.get('email', None)
+        if email is None:
+            return render(request, 'user/login.html')
+
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            return render(request, 'user/login.html')
+
+        file = request.FILES['file']
+        if file is None:
+            return Response(status=500)
+
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        user.thumbnail = uuid_name
+        user.save()
+
+        return Response(status=200, data=dict(uuid=uuid_name))
